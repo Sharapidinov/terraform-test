a9a7c29842ec404ccca2d2c4d0038d92bec0b0a6
Sharapidinov <austie2204@gmail.com>
2022-12-20 15:32:22 +0600

    first commit

---
+++ hello-world/hello.js
@@ -0,0 +1,18 @@
+// Lambda function code
+
+module.exports.handler = async (event) => {
+  console.log('Event: ', event)
+  let responseMessage = 'Success';
+
+
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+    }),
+  }
+}
